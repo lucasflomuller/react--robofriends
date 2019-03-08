4cec2b5fc3ec5641b33811e18b024b1bb05f8390
Lucas Muller <lucasflorianomuller@gmail.com>
2019-03-08 14:59:01 -0300

    implement redux thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,55 +6,46 @@ import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      robots: []
-    };
-  }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(users => {
-        this.setState({ robots: users });
-      });
+    this.props.onRequestRobots();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot =>
       robot.name.toLowerCase().includes(searchField.toLowerCase())
     );
-    if (!robots.length) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1>Robofriends</h1>
-          <SearchBox searchChange={onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={filteredRobots} />;
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      );
+
+    return isPending ?
+      <h1>Loading...</h1> :
+      (
+      <div className="tc">
+        <h1>Robofriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+      )
     }
-  }
 }
 
 const mapStateToProps = state => ({
-  searchField: state.searchField
+  searchField: state.searchRobots.searchField,
+  robots: state.requestRobots.robots,
+  isPending: state.requestRobots.isPending,
+  error: state.requestRobots.error
 });
 
 const mapDispatchToProps = dispatch => ({
-  onSearchChange: event => dispatch(setSearchField(event.target.value))
+  onSearchChange: event => dispatch(setSearchField(event.target.value)),
+  onRequestRobots: () => dispatch(requestRobots())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
