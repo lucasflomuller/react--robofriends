f9ef0e3a5c49ffdfbc72553d3de9fe7e5be41c54
lucasflomuller <lucasflorianomuller@gmail.com>
2019-03-29 18:37:35 +0000

    Updates

---
+++ precache-manifest.1ae02fec880f380290e7c7f93d9b52c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3e3994403e1e9f97b3",
+    "url": "/react--robofriends/static/js/runtime~main.b3e0807e.js"
+  },
+  {
+    "revision": "716342cd30b0b309115a",
+    "url": "/react--robofriends/static/js/main.dc2073c9.chunk.js"
+  },
+  {
+    "revision": "afda1326a3612dcf4d9f",
+    "url": "/react--robofriends/static/js/2.3323b336.chunk.js"
+  },
+  {
+    "revision": "716342cd30b0b309115a",
+    "url": "/react--robofriends/static/css/main.5b35be61.chunk.css"
+  },
+  {
+    "revision": "afda1326a3612dcf4d9f",
+    "url": "/react--robofriends/static/css/2.e5622839.chunk.css"
+  },
+  {
+    "revision": "704128636cbde5fbbbee09d1cf436a13",
+    "url": "/react--robofriends/index.html"
+  }
+];
