14aebe4edbe32d6bc9843053368ff3fa9b96e5c9
lucasflomuller <lucasflorianomuller@gmail.com>
2019-03-29 18:56:28 +0000

    add label to input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from "react";
 const SearchBox = ({searchField, searchChange}) => {
   return (
     <div className="pa2">
+      <label htmlFor="searchBoxField">Search robots:</label>
       <input
+        id="searchBoxField"
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
