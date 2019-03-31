ed4327b58abd3c458de85679323381ea6bab089a
lucasflomuller <lucasflorianomuller@gmail.com>
2019-03-31 03:07:29 +0000

    add test on CounterButton component

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -19,7 +19,7 @@ class CounterButton extends Component {
 
   render() {
     return (
-      <button color={this.props.color} onClick={this.updateCount}>
+      <button id='counter' color={this.props.color} onClick={this.updateCount}>
         Count: {this.state.count}
       </button>
     );
